e7c91feac51a133152d0b5f8ba34a781b57093e3
sadik126 <sadikhimel04@gmail.com>
2024-02-28 00:39:50 +0600

    practice js

---
+++ first.js
@@ -0,0 +1,39 @@
+// console.log('hello js sadik');
+
+// var num1= 0.3;
+// var num2 = 0.4;
+// var sum1 = num1 + num2;
+// console.log(sum1)
+
+//for even number (জোড় সংখ্যা)
+// var roast =  0;
+//  while (roast<=10) {
+//     console.log('roast den')
+//     console.log(roast)
+//     roast = roast + 2;
+    
+//  }
+
+ //for odd  number (বিজোড় সংখ্যা)
+//  var roast =  1;
+//  while (roast<=10) {
+//     console.log('roast den')
+//     console.log(roast)
+//     roast = roast + 2;
+    
+//  }
+
+// same example for loop
+//  for(var roast = 1; roast <=10 ; roast=roast+2){
+//  console.log(roast) 
+//  }
+
+// এখানে সব array  এর মান দেখতে চাইলে এভাবে বের করতে পারি for loop এর মাধ্যমে 
+
+var numbers = [23 , 45 , 458 , 85];
+
+for (let index = 0; index <= 3; index++) {
+    const element = numbers[index];
+    console.log(element)
+    
+}
